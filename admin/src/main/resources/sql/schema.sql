805b93cbaed3ef90f0255159859d3cc9466e97c3
MerceMay <n15697952078@gmail.com>
2025-12-03 21:48:56 +0800

    使用ShardingSphere实现分表

--- admin/src/main/resources/sql/schema.sql
+++ admin/src/main/resources/sql/schema.sql
@@ -11,6 +11,6 @@ CREATE TABLE `t_user`
     `update_time`   datetime     DEFAULT NULL COMMENT '修改时间',
     `del_flag`      tinyint(1)   DEFAULT NULL COMMENT '删除标识 0：未删除 1：已删除',
     PRIMARY KEY (`id`),
-    UNIQUE KEY `uk_username` (`username`)
+    UNIQUE KEY `uk_username` (`username`) USING BTREE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
