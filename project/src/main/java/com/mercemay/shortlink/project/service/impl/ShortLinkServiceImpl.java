9eff4f87f55f3fc135902c0b0b9f49f7524fcf3b
MerceMay <n15697952078@gmail.com>
2025-12-05 17:08:41 +0800

    使用当前时间戳实现重复链接创建

--- project/src/main/java/com/mercemay/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/mercemay/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -1,6 +1,8 @@
 package com.mercemay.shortlink.project.service.impl;
 
-import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.text.StrBuilder;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.mercemay.shortlink.project.common.convention.exception.ServiceException;
 import com.mercemay.shortlink.project.dao.entity.ShortLinkDO;
@@ -28,17 +30,34 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
     @Override
     public ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam) {
         String shortLinkSuffix = generateSuffix(requestParam);
-        String fullShortUrl = requestParam.getDomain() + "/" + shortLinkSuffix;
-        ShortLinkDO shortLinkDO = BeanUtil.toBean(requestParam, ShortLinkDO.class);
-        shortLinkDO.setShortUri(shortLinkSuffix);
-        shortLinkDO.setEnableStatus(0);
-        shortLinkDO.setFullShortUrl(fullShortUrl);
+        String fullShortUrl = StrBuilder.create(requestParam.getDomain())
+                .append("/")
+                .append(shortLinkSuffix)
+                .toString();
+        ShortLinkDO shortLinkDO = ShortLinkDO.builder()
+                .domain(requestParam.getDomain())
+                .originUrl(requestParam.getOriginUrl())
+                .gid(requestParam.getGid())
+                .createdType(requestParam.getCreatedType())
+                .validDateType(requestParam.getValidDateType())
+                .validDate(requestParam.getValidDate())
+                .describe(requestParam.getDescribe())
+                .shortUri(shortLinkSuffix)
+                .fullShortUrl(fullShortUrl)
+                .enableStatus(0)
+                .build();
         try {
             baseMapper.insert(shortLinkDO);
         } catch (DuplicateKeyException e) {
-            log.warn("短链接：{} 已存在，重新生成短链接后重试", fullShortUrl);
-            throw new ServiceException("短链接已存在，请稍后重试");
+            LambdaQueryWrapper<ShortLinkDO> queryWrapper = Wrappers.lambdaQuery(ShortLinkDO.class)
+                    .eq(ShortLinkDO::getFullShortUrl, fullShortUrl);
+            ShortLinkDO existingShortLink = baseMapper.selectOne(queryWrapper);
+            if (existingShortLink != null) {
+                log.warn("短链接：{} 已存在", fullShortUrl);
+                throw new ServiceException("短链接已存在，请稍后重试");
+            }
         }
+        shortUriCreateCachePenetrationBloomFilter.add(fullShortUrl);
         return ShortLinkCreateRespDTO.builder().
                 fullShortUrl(shortLinkDO.getFullShortUrl())
                 .originUrl(requestParam.getOriginUrl())
@@ -54,13 +73,13 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 throw new ServiceException("短链接生成失败，请稍后重试");
             }
             String originUrl = requestParam.getOriginUrl();
+            originUrl += System.currentTimeMillis();
             shortUri = HashUtil.hashToBase62(originUrl);
             if (!shortUriCreateCachePenetrationBloomFilter.contains(requestParam.getDomain() + "/" + shortUri)) {
                 break;
             }
             retryCount++;
         }
-        shortUriCreateCachePenetrationBloomFilter.add(requestParam.getDomain() + "/" + shortUri);
         return shortUri;
     }
 }
