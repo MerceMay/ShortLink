3dac30aaa0cea34fb021f285953d4d80e70f9b11
MerceMay <n15697952078@gmail.com>
2025-12-08 15:26:36 +0800

    新增链接设备统计功能及相关数据库支持

--- project/src/main/java/com/mercemay/shortlink/project/util/LinkUtil.java
+++ project/src/main/java/com/mercemay/shortlink/project/util/LinkUtil.java
@@ -96,4 +96,19 @@ public class LinkUtil {
             return "Unknown";
         }
     }
+
+    /**
+     * 获取设备类型
+     *
+     * @param request HttpServletRequest对象
+     * @return 设备类型
+     */
+    public static String getDevice(HttpServletRequest request) {
+        String userAgent = request.getHeader("User-Agent");
+        if (userAgent.toLowerCase().contains("mobile")) {
+            return "Mobile";
+        } else {
+            return "PC";
+        }
+    }
 }
